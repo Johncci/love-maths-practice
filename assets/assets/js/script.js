fe4f79aad55ec0690b9e57f43bd7335283591f97
John_considine <johnconsidine23@yahoo.co.uk>
2022-08-31 18:33:20 +0000

    add js

--- assets/assets/js/script.js
+++ assets/assets/js/script.js
@@ -1,1 +1,47 @@
-console.log("connected")
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "submit") {
+                console.log("you clicked submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                console.log(`you clicked ${gameType}`);
+
+            }
+        })
+    }
+})
+
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaysubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
